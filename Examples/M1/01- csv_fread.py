354f9898d0b97de96041dbdb7cda9f93ff2ef23e
danr844 <60269948+danr844@users.noreply.github.com>
2022-08-17 23:37:05 -0500

    print_results and load_data functions created

--- Examples/M1/01- csv_fread.py
+++ Examples/M1/01- csv_fread.py
@@ -7,6 +7,58 @@ import sys
 import config as cf
 import os
 
+#load pokemon file, reading, looping through the fie
+def load_data(i):
+
+        print("\n--- Joining filepath ---")
+        # subfolder name
+        pokemon_fn = "Pokemon-utf8-sample.csv"
+        # join pokemon file path
+        pokemon_path = os.path.join(cf.data_dir,
+                                pokemon_fn)
+        print("Pokemon filepath:", pokemon_path)    
+        # reading pokemon file
+        print("+++++++ Reading Pokemon file with csv.DictReader +++++++")
+        pokemon_file = open(pokemon_path, "r", encoding="utf-8")
+        # reader
+        pokemons = csv.reader(pokemon_file, delimiter=",")
+        # row counter
+        i = 0
+        pocket_cols = list()
+        pokemon_list_values = list()
+
+        # looping through pokemon file
+        for mon in pokemons:
+            if i == 0:
+                # getting pokemon columns
+                pocket_cols = mon
+            elif i != 0:
+                pokemon_list_values.append(mon)
+            i = i + 1
+
+        # pokemon file details
+        print("\n--- Pokemon csv details ---")
+        print("Pokemon file size:", i)
+        print("Pokemon columns:\n", pocket_cols)
+        print("////////////////////////////////////////////////////////////////////////////////////////")
+        print("////////////////////////////////////////////////////////////////////////////////////////")
+
+        # close pokemon file
+        pokemon_file.close()
+        return (pocket_cols,pokemon_list_values)
+
+def print_results(titulos, valores, N):
+    i=0
+    for x in range(0,N):
+        pokemon = "" 
+        for z in range(0, len(titulos)):
+            pokemon +=titulos[z]+ ": "+ valores[x][z]+" "
+        print(pokemon,"\n")
+
+
+
+
+#Method main
 if __name__ == "__main__":
     # start main()
     print("==========================================================")
@@ -26,67 +78,11 @@ if __name__ == "__main__":
     # config field size
     csv.field_size_limit(CUR_MAX_SIZE_FIELD)
 
-    print("\n--- Joining filepath ---")
-    # subfolder name
-    file_folder = "Samples"
-    pokemon_fn = "Pokemon-utf8-sample.csv"
-    # join pokemon file path
-    pokemon_path = os.path.join(cf.data_dir,
-                                file_folder,
-                                pokemon_fn)
-    print("Pokemon filepath:", pokemon_path)
-
-    # reading pokemon file
-    print("+++++++ Reading Pokemon file with csv.DictReader +++++++")
-    pokemon_file = open(pokemon_path, "r", encoding="utf-8")
-    # dict reader
-    pokemons = csv.DictReader(pokemon_file, delimiter=",")
     # row counter
     i = 0
     N = 200
 
-    # looping through pokemon file
-    for mon in pokemons:
-        if i % N == 0.0:
-            # printing each N th row
-            print("i:", i,
-                  "type:", type(mon), "\n"
-                  "data:", mon)
-        i = i + 1
+    pokemon_lists = load_data(i)
+    print_results(pokemon_lists[0], pokemon_lists[1], 5)
 
-    # pokemon file details
-    print("\n--- Pokemon csv details ---")
-    print("Pokemon file size:", i)
-    print("Pokemon fieldnames:\n", pokemons.fieldnames)
-
-    # close pokemon file
-    pokemon_file.close()
-
-    # reading pokemon file
-    print("+++++++ Reading Pokemon file with csv.DictReader +++++++")
-    pokemon_file = open(pokemon_path, "r", encoding="utf-8")
-    # reader
-    pokemons = csv.reader(pokemon_file, delimiter=",")
-    # row counter
-    i = 0
-    pocket_cols = list()
-
-    # looping through pokemon file
-    for mon in pokemons:
-        if i == 0:
-            # getting pokemon columns
-            pocket_cols = mon
-        elif i % N == 0.0:
-            # printing each N th row
-            print("i:", i,
-                "type:", type(mon), "\n"
-                "data:", mon)
-        i = i + 1
-
-    # pokemon file details
-    print("\n--- Pokemon csv details ---")
-    print("Pokemon file size:", i)
-    print("Pokemon columns:\n", pocket_cols)
-
-    # close pokemon file
-    pokemon_file.close()
+    
--- Utils/config.py
+++ Utils/config.py
@@ -1,6 +1,5 @@
 import os
 import sys
-file_path = os.path.join(os.path.dirname(__file__), '..')
-file_dir = os.path.dirname(os.path.realpath('__file__'))
-sys.path.insert(0, os.path.abspath(file_path))
-# data_dir = os.path.join(file_dir, 'Data')
+
+data_dir = os.path.abspath( '/Data')
+sys.path.insert(0, os.path.abspath(data_dir))
